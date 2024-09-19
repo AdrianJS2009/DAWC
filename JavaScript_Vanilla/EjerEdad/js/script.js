33779b881b4c51cc112322544c5e0f35ee947802
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2024-09-19 18:08:30 +0200

    Añadido ejercicio de mayoria de edad

---
+++ JavaScript_Vanilla/EjerEdad/js/script.js
@@ -0,0 +1,29 @@
+/* CARGA DOM */
+
+const inputEdad = document.querySelector('#inputEdad');
+const respuesta1 = document.querySelector('#respuesta1');
+const respuesta2 = document.querySelector('#respuesta2');
+const respuesta3 = document.querySelector('#respuesta3');
+
+/* FUNCIONES */
+
+function calcularEdad() {
+  const edad = inputEdad.value;
+  if (edad >= 18) {
+    respuesta1.textContent = "Si, puedes beber alcohol";
+    respuesta2.textContent = "Si, puedes entrar en la fiesta";
+    respuesta3.textContent = "No, no tiene entrada gratis"
+  }
+  else {
+    respuesta1.textContent = "No, no puedes beber alcohol";
+    respuesta2.textContent = "No, no puedes entrar en la fiesta";
+    respuesta3.textContent = "Si, tiene entrada gratis"
+  }
+}
+
+function esNumero(dato) {
+  if (isNaN(dato)) {
+    return false;
+  }
+  return true;
+}
