d2a9aa235578239662a4ce9e231a4438a3ca991b
Adrián Jiménez Santiago <ajimsan2009@g.educaand.es>
2024-09-23 20:54:30 +0200

    added react proyect

--- JavaScript_Vanilla/EjerEdad/js/script.js
+++ JavaScript_Vanilla/EjerEdad/js/script.js
@@ -1,9 +1,20 @@
+
 /* CARGA DOM */
 
 const inputEdad = document.querySelector('#inputEdad');
 const respuesta1 = document.querySelector('#respuesta1');
 const respuesta2 = document.querySelector('#respuesta2');
 const respuesta3 = document.querySelector('#respuesta3');
+const botonEnviar = document.querySelector('#botonEnviar');
+
+
+
+
+/* EVENTOS */
+
+botonEnviar.addEventListener('click', calcularEdad);
+
+
 
 /* FUNCIONES */
 
@@ -15,16 +26,35 @@ function esNumero(dato) {
   }
  }
 
+ function imprimirDom (elementoDOM, mensaje) {
+  elementoDOM.textContent = mensaje;
+ }
+
+
+ function resetDom() {
+  respuesta1.textContent = "";
+  respuesta2.textContent = "";
+  respuesta3.textContent = "";
+ }
+
 function calcularEdad() {
   const edad = inputEdad.value;
+  if (!esNumero(edad)) {
+    alert("No has introducido un número")
+    resetDom();
+  } else {
   if (edad >= 18) {
-    respuesta1.textContent = "Si, puedes beber alcohol";
-    respuesta2.textContent = "Si, puedes entrar en la fiesta";
-    respuesta3.textContent = "No, no tiene entrada gratis"
+    imprimirDom(respuesta1, "Si, puedes beber alcohol");
+    imprimirDom(respuesta2, "Si, puedes entrar en la fiesta");
+    imprimirDom(respuesta3, "Si, tiene entrada gratis");
+  } else {
+    imprimirDom(respuesta1, "No, no puedes beber alcohol");
+    imprimirDom(respuesta2, "No, no puedes entrar en la fiesta");
+    imprimirDom(respuesta3, "No, no tiene entrada gratis");
   }
-  else {
-    respuesta1.textContent = "No, no puedes beber alcohol";
-    respuesta2.textContent = "No, no puedes entrar en la fiesta";
-    respuesta3.textContent = "Si, tiene entrada gratis"
-  }
+ }
+
+ function imprimirDom (elementoDOM, mensaje) {
+  elementoDOM.textContent = mensaje;
+ }
 }
